0911eb0d42fe661e2576c3ba34c20915aaae25e8
itsuki-n22 <kazenokaori22@gmail.com>
2021-02-21 02:16:48 +0900

    delete needless files like node_modules, public/packs,publick/assets

--- public/assets/shared/flash_fadeout-dfc82a3fb6e6cf9621e4541d882e24bde641f8dd0ac8d45e5689b6eedd4741fa.js
+++
@@ -1,4 +0,0 @@
-// 全てのflashを自動でfadeOutするようにしている。
-flashCloseInterval = setInterval(function(){ 
-  $('#flash-message').find(".alert").fadeOut(1000)
-}, 3000);
--- public/assets/users/show/change_favorite-57e255db60243913041606afe9df0e03637be73901dba35cd7dcde15f09dbe0e.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click','[id*="favorite-no-"]',function(){
-  let starHtml = $(this).find("button");
-  let starClass = starHtml.attr('class')
-  if(starClass == undefined || !starClass.match(/favorited/)  ){
-    starHtml.addClass('favorited')
-  } else {
-    starHtml.removeClass('favorited')
-  }
-});
